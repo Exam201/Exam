7d914650f9d976460e4076e8c38a733787799151
Exam201 <123749992+Exam201@users.noreply.github.com>
2023-03-07 12:02:02 +0000

    Correcting spelling mistake in "boostrap"

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ include 'Library/dbconnect.php';
 $conn = connect();
 
 ?>
-<link rel="Stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/boostrap.css">
+<link rel="Stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
 <script scr="bootstrap-5.3.0-alpha1-dist/js/bootstrap.js"></script>
