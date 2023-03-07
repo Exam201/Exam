e670717a6bfbc2847dccab83111e39bca406fc63
Exam201 <123749992+Exam201@users.noreply.github.com>
2023-03-07 12:21:38 +0000

    added a basic nav bar

--- Library/dbconnect.php
+++ Library/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
  function connect(){
-    $conn = mysqli_connect("localhost","root","","health_advice_group");
+    $conn = mysqli_connect("localhost","root","","health_advice_group"); //this connects to the database and returns a connection object to the variable $conn
     if(!$conn){
         die("Connection failed: ".mysqli_connect_error());
     }
--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
 include 'Library/dbconnect.php';
-$conn = connect();
+$conn = connect(); //this calls the connection function from the dbconnect.php file and returns the connection object to the variable $conn
 
 ?>
 <link rel="Stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
 <script scr="bootstrap-5.3.0-alpha1-dist/js/bootstrap.js"></script>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <img src="images/placeholder.png" alt="logo" style="width:75px; height:50px; padding-right: 10px;">
+    <a class="navbar-brand" href="index.php">Health Advice Group</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Forecast</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Air Quality</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Guidance</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Settings</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
