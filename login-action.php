6872e7fe4b9b8139687a33fa299105ee952546ce
Exam201 <123749992+Exam201@users.noreply.github.com>
2023-03-15 09:55:15 +0000

    setting up the login system

---
+++ login-action.php
@@ -0,0 +1,33 @@
+<?php
+    include "Library/dbconnect.php";
+    $conn = connect();
+    $query = "SELECT * FROM users WHERE username=?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+    if ($user) {
+        if (password_verify($_POST['password'], $user['password'])) { // password_verify() is a PHP function that checks if a password matches a hash
+            session_unset();
+            session_start();
+            $_SESSION['userid'] = $user['user_id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['admin'] = $user['admin'];
+            $_SESSION['profileimage'] = $user['profileimage'];
+            header('location: index.php');
+        }
+        else {
+            session_unset();
+            session_start();
+            $_SESSION['pass_login_fail'] = true;
+            header('location: login.php');
+        }
+    }
+    else {
+        session_unset();
+        session_start();
+        $_SESSION['username_login_fail'] = true;
+        header('location: login.php');
+    }
+?>
---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+$username_alert = "hidden";
+$username_invalid = "form-control";
+if (isset($_SESSION['username_login_fail'])) {
+    $username_alert = "";
+    $username_invalid = "form-control is-invalid";
+    unset($_SESSION['username_login_fail']);
+} // this sets the alert and invalid classes for the username field if the user has failed to login
+$pass_alert = "hidden";
+$pass_invalid = "form-control";
+if (isset($_SESSION['pass_login_fail'])) {
+    $pass_alert = "";
+    $pass_invalid = "form-control is-invalid";
+    unset($_SESSION['pass_login_fail']);
+} // this sets the alert and invalid classes for the password field if the user has failed to login
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign in</title>
+    <link rel="stylesheet" href="bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
+    <script scr="bootstrap-5.3.0-alpha1-dist/js/bootstrap.js"></script>
+</head>
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <img src="images/placeholder.png" alt="logo" style="width:75px; height:50px; padding-right: 10px;">
+    <a class="navbar-brand" href="index.php">Health Advice Group</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="forecast.php">Forecast</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Air Quality</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Guidance</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Sign up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Settings</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- end navbar -->
+<h1 style="margin-top: 1%; text-align: center; display:block">Welcome back, please sign in!</h1>
+
+<form action="login-action.php" method="POST">
+    <div class="mb-3">
+    <label for="inputUsername1" class="form-label" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block" >Username</label>
+    <input type="text" class="<?php echo "$username_invalid" ?>" id="InputUsername1" aria-describedby="usernameHelp" name="username" placeholder="Username" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%;" required>
+    <div id="Usernamehelp" class="form-text" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block" ></div>
+    <div class='alert alert-danger' role='alert' <?php echo "$username_alert" ?> style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block">Username does not exist</div>
+</div>
+  </div>
+  <div class="mb-3">
+    <label for="InputPassword1" class="form-label" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block">Password</label>
+    <input type="password" class="<?php echo "$pass_invalid" ?>" id="InputPassword1" name="password" placeholder="Password" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%;" required>
+    <div class='alert alert-danger' role='alert' <?php echo "$pass_alert" ?> style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block">Incorrect Password</div>
+    </div>
+  <div class="mb-3">
+    <input type="submit" class="btn btn-primary" class="form-control" style="margin-top: 1%; margin-left: auto; margin-right: auto; width: 20%; display:block">
+</div>
+</form>
+</body>
+</html>
